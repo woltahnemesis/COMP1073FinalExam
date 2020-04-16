0db5444fd2cdda106b7a46f5a92d45595fd7e8ec
woltahnemesis <60105338+woltahnemesis@users.noreply.github.com>
2020-04-16 16:31:55 -0400

    Update q2.js

--- Q2/q2.js
+++ Q2/q2.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+//Main class 
 class Person {
   constructor(name, age, gender, classes) {
     this.name = name;
@@ -8,25 +9,33 @@ class Person {
     this.classes = classes;
   }
 
+//Method that greets when it's invoked    
   greeting() {
     console.log("Hi! I'm " + this.name);
   };
 
+//Method that says gooybye when it's invoked
   bye() {
     console.log(this.name + ' has left the building. Bye for now!');
   };
 }
 
-
+//Instantiating the object called Person
 let parth = new Person('Parth', 20, 'male', ['JavaScript', 'Java', 'PHP']);
 let harmanpreet = new Person('Harmanpreet', 22, 'male', ['JavaScript', 'C#', 'Relational DataBase']);
 
 
+//This is a subclass that's inheriting the main class' properties
 class Teacher extends Person {
-  constructor(name, last, age, gender, interests, subject, grade) {
-    super(name, age, gender, interests);
+    //2 properties were added
+  constructor(name, last, age, gender, classes, subject, grade) {
+    super(name, age, gender, classes);
     // subject and grade are specific to Teacher
+    this.last = last;
     this.subject = subject;
     this.grade = grade;
   }
 }
+
+//Instantiating the teacher
+let rich = new Teacher('Rich', 'Freeman', 25, 'male', ['ASP.net', 'PhP'], 'BootStrap', 99.01);
